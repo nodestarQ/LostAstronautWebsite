4e6d0480c6065874b962414006940d8dfc798ce5
Danish.K <danish.kiani@students.hs-mainz.de>
2022-09-17 03:49:49 +0200

    Updated LOST ASTRONAUTS MINT site

--- js/minttool.js
+++ js/minttool.js
@@ -1,7 +1,7 @@
 import {scABI} from "./scABI.js";
 import "./ethers.min.js";
 
-const scAddress = "SMART CONTRACT ADDRESS IN HERE";
+const scAddress = "0x1DeB1cf3Ec6A1F10808b59a0679c079D0dc4A5AC";
 var provider, account, signer, contract;
 const abashoPrice = 0.01;
 const regularPrice = 0.04;
@@ -16,7 +16,7 @@ async function connectAccounts() {
             scAddress,
             scABI.abi,
             signer);
-        walletConnected();
+        await walletConnected();
     } else {
         // metamask not available, lets get another provider
         console.log("No provider :-(");
@@ -31,7 +31,7 @@ async function connectAccounts() {
                 scAddress,
                 scABI.abi,
                 signer);
-            walletConnected();
+            await walletConnected();
         });
     }
 }
@@ -47,45 +47,61 @@ async function totalSupply(){
     }
   }
 
-function walletConnected() {
-    document.getElementById("ConnectWalletBtn").classList.remove("btn-primary");
-    document.getElementById("ConnectWalletBtn").classList.add("btn-outline-success");
-    document.getElementById("ConnectWalletBtn").innerHTML = "WALLET CONNECTED";
+async function walletConnected() {
+        document.getElementById("ConnectWalletBtn").classList.remove("btn-dark");
+        document.getElementById("ConnectWalletBtn").classList.add("btn-outline-dark");
+        document.getElementById("ConnectWalletBtn").innerHTML = "WALLET CONNECTED";
+        
+        document.getElementById("regularMintBtn").classList.remove("disabled");
+        document.getElementById("abashoMintBtn").classList.remove("disabled");
     
-    document.getElementById("regularMintBtn").classList.remove("disabled");
-    document.getElementById("abashoMintBtn").classList.remove("disabled");
+        updateMintCount();
 
-    updateMintCount();
+    
 }
 
 async function mintRegular() {
     if (window.ethereum) {
-        try {
-            const mint = await contract.recoverAstronaut({value: ethers.utils.parseEther(regularPrice.toString())});
-            updateMintCount();
-        } catch(err) {
-            alert("Something went wrong!");
-            console.log(err);
+        if(await contract.riftOpen()) {
+            try {
+                const mint = await contract.recoverAstronaut({value: ethers.utils.parseEther(regularPrice.toString())});
+                updateMintCount();
+            } catch(err) {
+                document.getElementById("alerts").classList.remove("visually-hidden");
+                document.getElementById("alerts").innerHTML = "Something went wrong";
+                console.log(err);
+            }
+        }
+        else {
+            document.getElementById("alerts").classList.remove("visually-hidden");
+            document.getElementById("alerts").innerHTML = "Regular Mint did not start yet...";
         }
     }
 }
 
 async function mintAbasho() {
     if (window.ethereum) {
-        try {
-            const mint = await contract.abashoRecoverAstronaut(parseInt(document.getElementById("abashoID").value), {value: ethers.utils.parseEther(abashoPrice.toString())});
-            updateMintCount();
-        } catch(err) {
-            alert("Something went wrong!");
-            console.log(err);
+        if(await contract.riftOpenAbasho()) {
+            try {
+                const mint = await contract.abashoRecoverAstronaut(parseInt(document.getElementById("abashoID").value), {value: ethers.utils.parseEther(abashoPrice.toString())});
+                updateMintCount();
+            } catch(err) {
+                document.getElementById("alerts").classList.remove("visually-hidden");
+                document.getElementById("alerts").innerHTML = "Something went wrong";
+                console.log(err);
+            }
+        }
+        else {
+            document.getElementById("alerts").classList.remove("visually-hidden");
+            document.getElementById("alerts").innerHTML = "Abasho Mint did not start yet...";
         }
     }
 }
 
 
-function updateMintCount() {
+async function updateMintCount() {
     document.getElementById("mintAmount").classList.remove("visually-hidden");
-    document.getElementById("mintAmount").innerHTML = totalSupply() + "/250";
+    document.getElementById("mintAmount").innerHTML = await totalSupply() + "/250";
 }
 
 
